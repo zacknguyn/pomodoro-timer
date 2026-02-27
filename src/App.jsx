947782780e32ca7cd11360951404578f8411ed4a
phong <quangphongnguyen147@gmail.com>
2026-02-27 17:39:40 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import TodoList from './components/TodoList.jsx'
+
+function App() {
+
+  return (
+    <>
+      <TodoList />
+    </>
+  )
+
+}
+
+export default App
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TodoList = () => {
+  return (
+    <div className="min-w-50 max-w-80 h-80 w-80 border-8">
+    </div>
+  )
+}
+
+export default TodoList;
